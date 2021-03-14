ad8e1192bf8c43c1013e5958b2d8634f04b5ae51
Steven Barnhurst <stevenbarnhurst@gmail.com>
2021-03-14 11:55:07 -0500

    Add login route as home

---
+++ lib/login/login.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        height: 150.0,
+        width: 190.0,
+        padding: EdgeInsets.only(top: 40),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(200),
+        ),
+        child: Center(
+          child: Image.network('https://raw.githubusercontent.com/yogitakumar/logindemo/master/asset/images/flutter-logo.png'),
+        ),
+      ),
+    );
+  }
+}
